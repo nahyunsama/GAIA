d0d287833dee32740bac39ee65f9c08dca085615
hamnahyun <hamnahyun@outlook.com>
2023-12-09 12:29:56 +0000

    json parser read file

--- C/json_parse/json_parse.c
+++ C/json_parse/json_parse.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include "cJSON.h"
+
+int main()
+{
+    FILE *fp = fopen("example.json", "r");
+    if (fp == NULL) {
+        printf("READ JSON FILE ERROR");
+        return 1;
+    }
+
+    fclose(fp);
+    return 0;
+}
