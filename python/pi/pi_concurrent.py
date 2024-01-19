dc4a13b2ad4bac988a8bad2a26264e5a8e8ad271
hamnahyun <hamnahyun@nyahyun.com>
2024-01-19 04:02:48 +0000

    add comment and worker

--- python/pi/pi_concurrent.py
+++ python/pi/pi_concurrent.py
@@ -1,5 +1,7 @@
 # machin formula
 # signma k=1 k=X {(16(-1)^(k-1))/(2k-1) * (1/5)^(2k-1) - 4(-1)^(k-1))/(2k-1) * (1/239)^(2k-1)} = pi/4
+# 1000 개씩 result 와 i 값을 저장해서 일시 중단 가능하게 만들기
+# windows docker command : docker run -v ${PWD}:/mnt --rm -it python:3 python3 /mnt/pi_concurrent.py
 
 from decimal import Decimal, getcontext
 from concurrent.futures import ProcessPoolExecutor
@@ -24,7 +26,7 @@ def main():
     ## map은 array 값을 변경하는 함수, range 1, REPEAT+1 의 값을 cal_pi 를 이용해서 변환
     ## ProcessPool 을 사용해서 GIL의 개입을 최대한 줄임 >> 장기적으로 GIL이 사라질 예정
 
-    with ProcessPoolExecutor(max_workers=12) as executor:
+    with ProcessPoolExecutor(max_workers=24) as executor:
         result = reduce(lambda x, y: x + y, executor.map(cal_pi, range(1, REPEAT+1)))
         #print(pi)
 
