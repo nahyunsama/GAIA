34d59e886f5d65c91dbc689974ddec0b69c59080
hamnahyun <hamnahyun@nyahyun.com>
2024-08-21 15:57:48 +0900

    create data with JPA, H2

---
+++ JAVA/spring_boot/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,35 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity // DB가 해당 객체를 인식
+public class Article {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
---
+++ JAVA/spring_boot/firstproject/src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.entity.Article;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleRepository extends CrudRepository<Article,Long> {
+
+
+}
