28152bfce5bbe784ef50063ea05807966f060f07
hamnahyun <hamnahyun@nyahyun.com>
2024-08-23 14:55:39 +0900

    Read data at webpage

--- JAVA/spring_boot/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ JAVA/spring_boot/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,7 +6,9 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -27,16 +29,30 @@ public class ArticleController {
         log.info(form.toString());
         //System.out.println(form.toString()); >> logging 으로 대체
 
-        // 1. DTO 를 Entity! 로 변환
+        // 1: DTO 를 Entity! 로 변환
         Article article = form.toEntity();
         log.info(article.toString());
         //System.out.println(article.toString());
 
-        // 2. Repostitory에게 Entity를 DB 안에 저장하게 함!
+        // 2: Repostitory에게 Entity를 DB 안에 저장하게 함!
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
         //System.out.println(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        // 1: id로 데이터를 가져옴
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2: 가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        // 3: 보여줄 페이지를 설정!
+        return "articles/show";
     }
 }
--- JAVA/spring_boot/firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ JAVA/spring_boot/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity // DB가 해당 객체를 인식
 @AllArgsConstructor
+@NoArgsConstructor // 디폴트 생성자를 추가해주는 어노테이션
 @ToString
 public class Article {
 
@@ -21,5 +23,4 @@ public class Article {
 
     @Column
     private String content;
-
 }
