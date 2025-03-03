91e17a1e1647faced10a0ce566185d6769ec7cd9
hamnahyun <hamnahyun@nahyun.com>
2025-03-03 17:08:59 +0900

    String class start

---
+++ JAVA/Vanilla/10_String_Class/01_RefCompare/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String str1 = new String("자바프로그래밍");
+        String str2 = new String("자바프로그래밍");
+        String str3 = "자바프로그래밍";
+        String str4 = "자바프로그래밍";
+
+        if (str1 == str2)
+            System.out.println("str1과 str2는 동일 객체 참조");
+        else
+            System.out.println("str1과 str2는 다른 객체 참조");
+
+        if (str3 == str4)
+            System.out.println("str3과 str4는 동일 객체 참조");
+        else
+            System.out.println("str3과 str4는 다른 객체 참조");
+    }
+}
