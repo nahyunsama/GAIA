2d0be572abb201b50675d37bdbb4ed0fb4afea85
hamnahyun <hamnahyun@nahyun.com>
2025-03-04 20:48:35 +0900

    Stringbuilder Tokenizer

--- JAVA/Vanilla/10_String_Class/08_StringTokenizer/src/main/java/org/example/Main.java
+++ JAVA/Vanilla/10_String_Class/08_StringTokenizer/src/main/java/org/example/Main.java
@@ -5,10 +5,14 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) {
         StringTokenizer str1 = new StringTokenizer("a b c");
-        //StringTokenizer str2 = new StringTokenizer("1,2,3",",");
+        StringTokenizer str2 = new StringTokenizer("1,2,3",",");
 
         while (str1.hasMoreTokens()) { //토큰 유무 확인
             System.out.println(str1.nextToken());
+        }
+
+        while (str2.hasMoreTokens()) { //토큰 유무 확인
+            System.out.println(str2.nextToken());
         }
     }
 }
