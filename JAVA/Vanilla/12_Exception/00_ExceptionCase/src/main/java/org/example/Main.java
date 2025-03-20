598b814ee6ef4fb7bcbad7169af7ebff1645459d
hamnahyun <hamnahyun@nahyun.com>
2025-03-20 21:52:58 +0900

    general exception and execute exception

---
+++ JAVA/Vanilla/12_Exception/00_ExceptionCase/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = 10 / num1;
+        System.out.println(num2);
+
+        //MyBook book1 = new MyBook();
+    }
+}
